92f441a765158726310137215a70a65b96bb4742
pedromm65 <pedromm65@hotmail.com.br>
2021-12-15 00:42:19 -0300

    Criação da rota para cadastro.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
+app.use(express.json())
+
+const customers = []
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body
+    const accountAlreadyExist = customers.some(customer => customer.cpf === cpf)
+    if(accountAlreadyExist) {
+        return response.status(400).json({ error: "Customer already exists!" })
+    } 
+  
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send()     
+})
 
 app.listen(3333, () => {
     console.log("Servidor Online na porta 3333!")
